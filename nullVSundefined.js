331ff95eb2cf7c385913074d7941530d3f6cda7a
fem97 <fem.nwu@gmail.com>
2021-02-07 11:48:57 +0600

    null vs undefined

---
+++ nullVSundefined.js
@@ -0,0 +1,22 @@
+let myName;
+console.log(myName);
+
+function add(x, y) {
+    console.log(y);
+}
+add(10)
+
+function add(x, y) {
+    console.log(x + y);
+}
+var result = add(10, 10);
+console.log(result);
+
+var obj = {name: 'fahim', age: 23};
+console.log(obj.id);
+
+var arr = [10, 20];
+console.log(arr[5]);
+
+let x = null;
+console.log(x);
