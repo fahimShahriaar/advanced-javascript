3a2e830e481ce387aef57b65cfc13931e8fa57c0
fem97 <fem.nwu@gmail.com>
2021-02-07 11:27:58 +0600

    truthy value VS falsy  value

--- truthyValueVSfalsyValue.js
+++ truthyValueVSfalsyValue.js
@@ -1,6 +1,7 @@
 // All positive and negative numbers are true - Except 0
 // All strings return true. Empty string returns false
 // undefined, NaN, null return false
+// Empty array returns true
 
 if ([]) {
     console.log('The condition is true');
